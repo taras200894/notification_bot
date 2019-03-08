5428dc03453cd2d7ce4ed15a1116f9a6b6cbe8b4
Taras Stopenchuk <taras.stopenchuk@radarservices.com>
2019-03-08 17:35:50 +0100

    Add check OM and send to telegram

--- lib/notification_bot/bot.ex
+++ lib/notification_bot/bot.ex
@@ -7,6 +7,15 @@ defmodule NotificationBot.Bot do
 
   def handle({:command, "start", msg}, _name, _extra) do
     IO.inspect(msg)
-    answer("Hi! I'm a nice bot :D")
+    {_, [first_car | _tail]} = NotificationBot.Scraper.get_cars
+    first_car = Map.from_struct(first_car)
+
+    answer(generate_string_response(first_car))
+  end
+
+  defp generate_string_response(car) do
+    response = Enum.map(car, fn {a, i} -> "#{Atom.to_string(a)}: #{i}" end)
+
+    Enum.join(response, ", ")
   end
 end
